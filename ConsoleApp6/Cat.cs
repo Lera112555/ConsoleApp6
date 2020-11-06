b0835e0bfca95122f11f0554a0ece429287c9df9
Lera112555 <73837980+Lera112555@users.noreply.github.com>
2020-11-06 15:56:17 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Cat.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Cat
+    {
+        
+        
+            byte _hungryStatus;
+            public Cat(string name, DateTime birthday)
+            {
+                Name = name;
+                BirthDay = birthday;
+                Task.Run(LifeCircle);
+            }
+            public string Name
+            {
+                get; set;
+            }
+            public DateTime BirthDay
+            {
+                get; set;
+            }
+            public void MakeNoise()
+            {
+                Console.WriteLine(Name + " мяукает");
+            }
+            public int GetAge()
+            {
+                return (DateTime.Today - BirthDay).Days / 365;
+            }
+
+            public void GetStatus()
+            {
+                Console.WriteLine(Name);
+                Console.WriteLine($"Возрост: {GetAge()}");
+                if (HungryStatus < 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Кот умерает от голода ");
+                }
+                else if (HungryStatus >= 10 && HungryStatus <= 40)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Кот очень голоден");
+                }
+                else if (HungryStatus > 40 && HungryStatus <= 70)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine($"Кот хочет кушать");
+                }
+                else if (HungryStatus > 70 && HungryStatus <= 90)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Кот не против перекусить");
+                }
+                else if (HungryStatus > 90)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Кот недавно поел");
+                }
+                Console.ResetColor();
+            }
+            async Task LifeCircle()
+            {
+                await Task.Delay(1000);
+                HungryStatus -= 10;
+                await LifeCircle();
+            }
+            public void Feed()
+            {
+                HungryStatus = 100;
+            }
+            public event EventHandler HungryStatusChanged;
+            public byte HungryStatus
+            {
+                get { return _hungryStatus; }
+                set
+                {
+                    if (value < 0)
+                        _hungryStatus = 0;
+                    else if (value > 100)
+                        _hungryStatus = 100;
+                    else
+                        _hungryStatus = value;
+                    if (_hungryStatus != value)
+                        HungryStatusChanged?.Invoke(this, null);
+
+                }
+            }
+        }
+    }
+
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Kit", new DateTime(2005, 8, 7));
+            Console.WriteLine($"Коту по имени {cat.Name} уже {cat.GetAge()} лет");
+            cat.HungryStatus = 150;
+            cat.HungryStatusChanged += Cat_HungryStatusChanged;
+            Cat cat2 = new Cat("Kit", new DateTime(2005, 8, 7));
+            Console.WriteLine($"Коту по имени {cat2.Name} уже {cat2.GetAge()} лет");
+            cat2.HungryStatusChanged += Cat2_HungryStatusChanged;
+            Console.ReadLine();
+
+        }
+
+        public static void Cat_HungryStatusChanged(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Cat cat = (Cat)sender;
+            if (cat.HungryStatus < 20 && rnd.Next(0, 10) < 5)
+                cat.Feed();
+            else
+                cat.GetStatus();
+        }
+        public static void Cat2_HungryStatusChanged(object sender, EventArgs e)
+
+        {
+            Random rnd = new Random();
+            Cat cat = (Cat)sender;
+            if (cat.HungryStatus < 20 && rnd.Next(0, 10) < 5)
+                cat.Feed();
+            else
+                cat.GetStatus();
+
+
+        }
+
+    }
+}
